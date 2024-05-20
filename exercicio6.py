380b6e5c678c8a87bfbacf14bbe4df5538aaef5e
RichardyBS <editionplays@gmail.com>
2024-05-20 15:07:29 -0300

    treino python

--- exercicio6.py
+++ exercicio6.py
@@ -1,8 +1,20 @@
 ##Escreva um programa que solicite números inteiros para o usuário. O programa deve ler os números até que o usuário digite 0 (zero). No final da execução, exiba a quantidade de números digitados, assim como a soma e a média aritmética. 
 
-solUser = int(input("Digite um Numero Inteiro"))
+numUser = int(input("Escreva um numero inteiro e irei somalo infinitamente. Digite 0 para Finalizar o Programa: "))
+resposta = 0
+soma = resposta + numUser
+media = soma / 2
+resposta += numUser;
 
-
+while numUser > 0:
+    if numUser > 0:
+        print(f"A soma dos numeros digitados foi: {resposta}")
+        numUser = int(input("Escreva um numero inteiro e irei somalo infinitamente. Digite 0 para Finalizar o Programa: "))
+    
+    if numUser == 0:
+        print("+", end=" ")
+        print(f" = {numUser + soma}\n", end="")
+        print(f" = {media}", end="")
 
 
 
